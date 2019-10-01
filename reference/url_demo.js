d963110389bdc029eaa5d6c9f883e76a3c621b69
agwa.dan <agwa.dan@gmail.com>
2019-10-01 11:21:04 +0300

    tut 00:43:00 - url

---
+++ reference/url_demo.js
@@ -0,0 +1,9 @@
+//URL MODULE
+
+const url = require('url');
+
+const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+
+//Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
